55b1a81ae4ee44bd9314355e416648e4350d8de7
ya.nematov <nematovmuhammadqodir68@gmail.com>
2026-02-10 19:55:27 +0900

    develop RideCard full component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,7 +1,8 @@
 import RideCard from "@/components/RideCard";
+import { images } from "@/constants";
 import { useUser } from "@clerk/clerk-expo";
 import React from "react";
-import { FlatList } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const recentRides = [
@@ -113,11 +114,33 @@ const recentRides = [
 
 const Home = () => {
   const user = useUser();
+  const loading = true;
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
-        data={recentRides}
+        data={[]}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        ListEmptyComponent={() => {
+          return (
+            <View className="flex flex-col items-center justify-center">
+              {!loading ? (
+                <>
+                  <Image
+                    source={images.noResult}
+                    resizeMode="contain"
+                    className="w-40 h-40"
+                    alt="No recent rides found!"
+                  />
+                  <Text className="text-sm"> No recent rides found!</Text>
+                </>
+              ) : (
+                <ActivityIndicator size="large" color="#000" />
+              )}
+            </View>
+          );
+        }}
       />
     </SafeAreaView>
   );
--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,3 +1,5 @@
+import { icons } from "@/constants";
+import { formatDate } from "@/libs/utils";
 import { Ride } from "@/types/type";
 import React from "react";
 import { Image, Text, View } from "react-native";
@@ -18,7 +20,7 @@ const RideCard = ({
 }) => {
   return (
     <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
-      <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-col items-center justify-between p-3">
         <View className="flex flex-row items-center justify-between">
           <Image
             source={{
@@ -26,9 +28,65 @@ const RideCard = ({
             }}
             className="w-[80px] h-[90px] rounded-lg"
           />
+          <View className="flex flex-col mx-5 gap-y-5 flex-1">
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.to} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {origin_address}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center gap-x-2">
+              <Image source={icons.point} className="w-5 h-5" />
+              <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+                {destination_address}
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <View className="flex flex-col w-full mt-5 bg-general-500 rounded-lg p-3 items-center justify-center">
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Date & Time
+            </Text>
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              {formatDate(created_at)} {formatDate(String(ride_time))}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Driver
+            </Text>
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              {driver.first_name} {driver.last_name}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Car Seats
+            </Text>
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              {driver.car_seats}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className="text-md font-JakartaMedium text-gray-500">
+              Payment Status
+            </Text>
+            <Text
+              className={`text-md font-JakartaMedium text-gray-500 capitalize ${
+                payment_status === "paid" ? "text-green-500" : "text-red-500"
+              }`}
+            >
+              {payment_status}
+            </Text>
+          </View>
         </View>
       </View>
-      <Text className="text-3xl">{driver.first_name}</Text>
     </View>
   );
 };
